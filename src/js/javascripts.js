1730dfa0577cf201ca6ca08d734d6d3dab570ca2
willfulAversion <eziosheva0000@gmail.com>
2017-07-02 23:57:10 +0300

    google api data and user github image

--- src/js/javascripts.js
+++ src/js/javascripts.js
@@ -2,6 +2,8 @@
 var globalData = {};
 // geolocation
 var btnPosition = document.getElementById('getPosition');
+let wrap = document.getElementsByClassName('output')[0];
+// listener
 btnPosition.addEventListener('click', getPosition)
 // get position
 function getPosition() {
@@ -26,14 +28,16 @@ function showPosition(position) {
   console.log(position);
   var infoWrap = document.getElementsByClassName('output')[0];
   infoWrap.innerHTML = `<span>${position.coords.latitude}</span> <span>${position.coords.longitude}</span>`
-  // var url = `https://api.darksky.net/forecast/f3acae9e479d32def347b5118e41bc60/${position.coords.latitude},${position.coords.longitude}`;
-  var url = `https://api.github.com/users/willfulAversion`;
-
-  fetch(url,{
-    // mode: 'no-cors',
+  var gitHubUserUrl = `https://api.github.com/users/willfulAversion`;
+  let apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyDlT_lL5an8JHAJjv4CuGbC5Rbp9oDCMKA`
+  // var weatherUrl = `https://api.darksky.net/forecast/f3acae9e479d32def347b5118e41bc60/${position.coords.latitude},${position.coords.longitude}`;
+  // google api data
+  getApiData(apiUrl);
+  // github user fetcch
+  fetch(gitHubUserUrl, {
     method: 'GET',
     headers: {
-    // Accept: 'application/json',
+    "Accept": "application/json"
   }
   }).then(
     response => {
@@ -42,13 +46,43 @@ function showPosition(position) {
       }
     }
   ).then(
-    (text) => {
-      console.log('all my data: ', JSON.parse(text))
+    (jsonGithub) => {
+      showUser(jsonGithub)
     }
   ).catch(
     (error) => {
       console.log(`Request faild ${error}`)
     }
   )
+}
+//
+function showUser(data) {
+  let parseData = JSON.parse(data);
+  let userImg = document.createElement('img');
 
+  userImg.setAttribute('class', 'userImg');
+
+  userImg.setAttribute('src', parseData.avatar_url);
+  wrap.appendChild(userImg);
+}
+
+// google API
+function getApiData(url) {
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      "Accept": 'application/json'
+    }
+  }).then(
+    (response) => {
+      if (response.ok) {
+        return response.text()
+      }
+    }
+  ).then(
+    (geolocationData) => {
+      console.log(geolocationData);
+
+    }
+  )
 }
